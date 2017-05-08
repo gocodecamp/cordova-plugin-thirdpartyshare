9884a5836ed84e75b5d8a32e72f6f10362c4870c
kungfuwukong <kungfuwukong@gmail.com>
2017-05-08 12:14:46 +0800

    客户端唤醒设置

--- src/android/UmengSocialPlugin.java
+++ src/android/UmengSocialPlugin.java
@@ -50,6 +50,9 @@ public class UmengSocialPlugin extends CordovaPlugin {
         PlatformConfig.setQQZone(qqId, qqKey);
 
         UMShareAPI.get(mActivity);
+        UMShareConfig config = new UMShareConfig();
+        config.isNeedAuthOnGetUserInfo(true);
+        UMShareAPI.get(mActivity).setShareConfig(config);
     }
 
 
